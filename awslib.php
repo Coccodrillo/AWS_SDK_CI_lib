0014aaaad67df577370b6341428cbb8cfa21e690
Rok Biderman <rok.biderman@interaktivnost.net>
2012-03-05 15:18:05 +0100

    Initial commit

---
+++ awslib.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');  
+
+class Awslib {
+
+    function Awslib()
+	{
+		require_once('aws-sdk-for-php/sdk.class.php');
+	}
+}
+
+/* End of file Awslib.php */
